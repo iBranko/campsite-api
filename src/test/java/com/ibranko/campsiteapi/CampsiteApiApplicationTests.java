2aa1e61854d6755a5cf8c3693268ff69f940b5f4
Paulo Avila <paulo.cerri@totvs.com.br>
2021-02-26 14:19:43 -0300

    -Added simple initial test. -Removed show-sql parameter. Not needed anymore.

--- src/test/java/com/ibranko/campsiteapi/CampsiteApiApplicationTests.java
+++ src/test/java/com/ibranko/campsiteapi/CampsiteApiApplicationTests.java
@@ -1,13 +1,35 @@
 package com.ibranko.campsiteapi;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpUriRequest;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@SpringBootTest
+import java.io.IOException;
+import java.util.UUID;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
 class CampsiteApiApplicationTests {
 
     @Test
-    void contextLoads() {
+    void givenBookingIdDoestNotExists_whenReservationIsRetrieved_then404IsReceived() throws IOException {
+        //Given
+        UUID bookingId = UUID.randomUUID();
+        HttpUriRequest request = new HttpGet("http://localhost:8080/reservations/" + bookingId);
+
+        //When
+        HttpResponse httpResponse = HttpClientBuilder.create().build().execute( request );
+
+        //Then
+        Assertions.assertEquals(httpResponse.getStatusLine().getStatusCode(),
+                HttpStatus.NOT_FOUND.value());
     }
 
 }
